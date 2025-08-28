51d026e91f81bb146a80746ecbe4f2c25520f989
MF <mf01000009120.com>
2025-08-28 23:20:57 +0300

    Complete architecture add edit Project Component

--- src/apps/dashboard/manger/projects/projects.module.ts
+++ src/apps/dashboard/manger/projects/projects.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
 import { AddEditProjectComponent } from './components/add-edit-project/add-edit-project.component';
-
+import { SharedModule } from 'src/apps/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    ProjectsComponent,
-    AddEditProjectComponent
-  ],
-  imports: [
-    CommonModule,
-    ProjectsRoutingModule
-  ]
+  declarations: [ProjectsComponent, AddEditProjectComponent],
+  imports: [CommonModule, ProjectsRoutingModule, SharedModule],
 })
-export class ProjectsModule { }
+export class ProjectsModule {}
