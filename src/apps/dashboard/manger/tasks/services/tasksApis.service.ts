23657c462597aca105682915289e86b6a37641fe
SamehWally <samehwaly27@gmail.com>
2025-08-30 16:52:53 +0300

    Add List Tasks View

--- src/apps/dashboard/manger/tasks/services/tasksApis.service.ts
+++ src/apps/dashboard/manger/tasks/services/tasksApis.service.ts
@@ -1,10 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TasksApisService {
+  constructor(private _HttpClient: HttpClient) {}
 
-constructor() { }
-
+  getAllTasks(paramsData: any): Observable<any> {
+    return this._HttpClient.get('Task/Manager', { params: paramsData });
+  }
 }
